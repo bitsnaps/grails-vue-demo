9279c89ded9acf4fa656e3cfbb48ec4dd724750b
bitsnaps <bitsnaps@yahoo.fr>
2019-05-24 16:13:42 +0100

    first commit

---
+++ server/grails-app/init/demo/BootStrap.groovy
@@ -0,0 +1,33 @@
+package demo
+
+import grails.compiler.GrailsCompileStatic
+
+@GrailsCompileStatic
+class BootStrap {
+
+    DriverDataService driverDataService
+    MakeDataService makeDataService
+    ModelDataService modelDataService
+    VehicleDataService vehicleDataService
+
+    def init = { servletContext ->
+
+        log.info "Loading database..."
+        Driver driver1 = driverDataService.save("Susan")
+        Driver driver2 = driverDataService.save("Pedro")
+
+        Make nissan = makeDataService.save("Nissan")
+        Make ford = makeDataService.save("Ford")
+
+        Model titan = modelDataService.save("Titan")
+        Model leaf = modelDataService.save("Leaf")
+        Model windstar = modelDataService.save("Windstar")
+
+        vehicleDataService.save("Pickup", driver1, nissan, titan)
+        vehicleDataService.save("Economy", driver1, nissan, leaf)
+        vehicleDataService.save("Minivan", driver2, ford, windstar)
+
+    }
+    def destroy = {
+    }
+}
---
+++ server/grails-app/services/demo/DriverDataService.groovy
@@ -0,0 +1,11 @@
+package demo
+
+import grails.gorm.services.Service
+
+
+@Service(Driver)
+interface DriverDataService {
+
+    Driver save(String name)
+
+}
